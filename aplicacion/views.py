7997da95a404a3a84dae6b48b37cb58b80a84c95
yoelpipeflay <fe.galan@duocuc.cl>
2023-06-10 01:36:34 -0400

    agregado el index a django

--- aplicacion/views.py
+++ aplicacion/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'aplicacion/index.html')
+
+def iniciosession(request):
+    return render(request, 'aplicacion/iniciosession.html')
