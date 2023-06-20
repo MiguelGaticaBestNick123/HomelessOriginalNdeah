f42b308392558297b219ef24eeca3b71161ff016
MiguelGaticaBestnickname123 <mi.gatica@duocuc.cl>
2023-06-19 21:12:17 -0400

    Merge arreglado

--- aplicacion/views.py
+++ aplicacion/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'aplicacion/index.html')
+
+def iniciosession(request):
+    return render(request, 'aplicacion/iniciosession.html')
+
+def notasmedicas(request):
+    return render (request, 'aplicacion/notasmedicas.html')
