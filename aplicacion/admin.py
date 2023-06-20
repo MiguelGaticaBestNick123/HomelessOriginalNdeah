45530b2693df40530392ff52197cf7c1f6df169d
MiguelGaticaBestnickname123 <mi.gatica@duocuc.cl>
2023-06-19 21:30:55 -0400

    Models modificado, otra vez
    otra vez... otra vezzZzZzZz

--- aplicacion/admin.py
+++ aplicacion/admin.py
@@ -1,39 +1,53 @@
 from django.contrib import admin
-from .models import User, Familiar, Aportador, Residente, Accion, Insumo, CompraInsumo, StockInsumo, Staff
+from .models import Familiar, Aportador, Residente, Accion, Insumo, CompraInsumo, StockInsumo
 
-class UserAdmin(admin.ModelAdmin):
-    # Personaliza la visualización y la edición del modelo User si es necesario
-    pass
+
 
 class FamiliarAdmin(admin.ModelAdmin):
     list_display = ('rut', 'nombre', 'apellido', 'f_nacimiento', 'n_telefono', 'email')
     list_editable = ('n_telefono', 'email')
+    class meta:
+        model = Familiar
+        
 
 class AportadorAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'apellido', 'direccion', 'correo', 'telefono', 'nro_tarjeta', 'sexo')
     list_editable = ('direccion', 'correo', 'telefono', 'nro_tarjeta')
+    class meta:
+        model = Aportador
 
 class ResidenteAdmin(admin.ModelAdmin):
     list_display = ('rut', 'nombre', 'apellido', 'f_nacimiento', 'sexo', 'tutor')
+    class meta:
+        model = Residente
 
 class CompraInsumoAdmin(admin.ModelAdmin):
     list_display = ('fecha_compra', 'cantidad', 'descripcion', 'insumo', 'proveedor', 'costo_unitario')
     list_editable = ('cantidad', 'descripcion', 'insumo', 'proveedor', 'costo_unitario')
+    class meta:
+        model = CompraInsumo
 
 class AccionAdmin(admin.ModelAdmin):
     list_display = ('residente', 'fecha_hora', 'descripcion')
     list_editable = ('fecha_hora', 'descripcion')
     list_display_links = ('residente',)  # Agrega esta línea
+    class meta:
+        model = Accion
+        
+class InsumoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'descripcion')
+    class meta:
+        model = Insumo
+
 
 class StockInsumoAdmin(admin.ModelAdmin):
     list_display = ('insumo', 'cantidad_stock')
     list_editable = ('cantidad_stock',)
     list_display_links = ('insumo',)  # Agrega esta línea
+    class meta:
+        model = StockInsumo
 
-class InsumoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nombre', 'descripcion')
 
-admin.site.register(User)
 admin.site.register(Familiar, FamiliarAdmin)
 admin.site.register(Aportador, AportadorAdmin)
 admin.site.register(Residente, ResidenteAdmin)
